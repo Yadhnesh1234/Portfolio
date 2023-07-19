6c96c67033991ae140775bbfc6d3bea758533b6b
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-07-19 19:16:04 +0530

    Responsiveness Changes Added

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,4 +1,3 @@
-// Landing.js
 import React from 'react';
 import logoImage from '../../assets/avatar.svg'; 
 import heroImage from '../../assets/devices.svg'; 
@@ -6,30 +5,28 @@ import heroImage from '../../assets/devices.svg';
 const Landing = () => {
   return (
     <>
-    <div className="bg-white h-screen flex items-center justify-center flex-col ">
-      <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-eurostile font-semibold text-center">
+    <div className="bg-white min-h-screen flex items-center justify-center flex-col p-4 md:p-8 lg:p-16">
+      <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-eurostile font-semibold">
         Full Stack Web Developer & Blogger
       </h1>
-      <p className="text-center text-sm md:text-lg lg:text-xl font-eurostile mt-8 text-center">
+      <p className="text-center text-sm md:text-lg lg:text-xl font-eurostile mt-4 md:mt-8">
         I design and code beautifully simple things, and I love what I do.
       </p>
       <img
         src={logoImage}
         alt=""
-        width="210"
-        height="229.34"
-        className="mt-12 w-auto h-auto"
+        className="mt-6 md:mt-12 w-52 md:w-60 lg:w-72 h-auto"
       />
       <img
         src={heroImage}
         alt=""
         width="860"
         height="348.75"
-        className="mt-24"
+        className="mt-8 md:mt-12 max-w-full h-auto"
       />
     </div>
     </>
   );
 };
 
-export default Landing
+export default Landing;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,27 +1,41 @@
-// Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-  return (
-    <nav className="bg-white p-8 flex items-center justify-around">
-      {/* Logo on the Left */}
-      <span className="font-bold text-2xl">Y LOGO</span>
-      <div></div>
-      {/* Blog and Say Hello Group */}
-      <div className="flex items-center space-x-4">
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className={`bg-white p-4 md:p-8 flex items-center justify-around relative z-10 ${isMenuOpen ? 'pb-16 md:pb-0' : ''}`}>
+      {/* Logo on the Left */}
+      <span className="font-bold text-xl">Y LOGO</span>
+
+      {/* Hamburger Menu */}
+      <div className="md:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-gray-800 hover:text-purple-600 transition duration-300 text-xl"
+        >
+          &#9776;
+        </button>
+      </div>
+
+      {/* Navigation Links */}
+      <div
+        className={`md:flex md:flex-row space-x-4 space-y-5 ${isMenuOpen ? 'flex flex-col items-center justify-center md:block' : 'hidden md:block'} absolute md:static bottom-[-100%] md:bottom-auto left-0 md:left-auto w-full md:w-auto bg-white md:bg-transparent md:space-x-0 z-0`}
+      >
         {/* Blog Link */}
-        <a href="/" className="text-gray-800 hover:text-purple-600 transition duration-300 text-xl">Blog</a>
+        <a href="/" className="block text-gray-800 hover:text-purple-600 transition duration-300 text-lg md:inline-block md:text-lg">Blog</a>
 
         {/* Say Hello Button */}
         <button
-          className="border border-purple-600 text-purple-600 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-purple-600 hover:text-white text-lg px-0 py-3 w-32"
+          className="border border-purple-600 text-purple-600 py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-purple-600 hover:text-white text-lg md:inline-block md:text-lg"
         >
           Say Hello
         </button>
-
       </div>
-
     </nav>
   );
 };
