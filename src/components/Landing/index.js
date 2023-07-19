72a49e9cc8820b0af34553ec7a589d80f735e7e3
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-07-19 18:35:12 +0530

    Responsive ness Added

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -7,10 +7,10 @@ const Landing = () => {
   return (
     <>
     <div className="bg-white h-screen flex items-center justify-center flex-col ">
-      <h1 className="text-5xl font-eurostile font-semibold">
+      <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-eurostile font-semibold text-center">
         Full Stack Web Developer & Blogger
       </h1>
-      <p className="text-2xl font-eurostile mt-8">
+      <p className="text-center text-sm md:text-lg lg:text-xl font-eurostile mt-8 text-center">
         I design and code beautifully simple things, and I love what I do.
       </p>
       <img
@@ -18,7 +18,7 @@ const Landing = () => {
         alt=""
         width="210"
         height="229.34"
-        className="mt-12"
+        className="mt-12 w-auto h-auto"
       />
       <img
         src={heroImage}
@@ -32,4 +32,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default Landing
