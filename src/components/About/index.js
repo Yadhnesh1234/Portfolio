5fafdb77da5ada99cb4022ccd370e06cc7a111a7
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-07-21 17:36:43 +0530

    About Section Added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,16 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="bg-purple-600 w-1838 h-612 p-144 md:p-72 lg:p-288 text-white text-center">
+    <div className="bg-purple-600 w-screen" >
+      <div className="flex flex-col justify-center items-center text-base p-20
+       lg:p-72 text-center">
       <h2 className="text-4xl font-bold">
         Hi, I’m Yadhnesh. Nice to meet you.
       </h2>
       <p className="text-lg mt-8">
         Since beginning my journey as a freelance designer over 11 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
       </p>
+      </div>
     </div>
   );
 };
