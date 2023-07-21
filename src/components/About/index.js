2d99ac5188a8e5f017f3312616faa0b0fec075f7
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-07-21 18:43:49 +0530

    Landing page changes Added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div className="bg-purple-600 w-screen h-600  relative top-0" >
+    <div className="bg-purple-600 w-screen h-600 text-white  relative top-0" >
       <div className="flex flex-col justify-center items-center text-base p-20
        lg:p-72 text-center">
       <h2 className="text-4xl font-bold">
         Hi, I’m Yadhnesh. Nice to meet you.
       </h2>
-      <p className="text-lg mt-8">
+      <p className="text-lg mt-8 ">
       A passionate Full Stack Web Developer. With expertise in HTML, CSS, JavaScript, Node.js, and Python, I craft captivating user interfaces and robust backends. A problem solver and team player, I love collaborating to bring ideas to life. Constantly learning and staying on top of trends, I'm committed to delivering exceptional results. Let's create something amazing together!
       </p>
       </div>
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -8,7 +8,7 @@ const Landing = () => {
       <h1 className="text-center text-3xl md:text-4xl lg:text-5xl  font-eurostile font-semibold">
         Full Stack Web Developer & Blogger
       </h1>
-      <p className="text-center text-sm md:text-lg lg:text-xl font-eurostile mt-4 md:mt-8">
+      <p className="text-center  text-sm md:text-lg lg:text-xl font-eurostile mt-4 md:mt-8">
         I design and code beautifully simple things, and I love what I do.
       </p>
       <img
