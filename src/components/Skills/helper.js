5a1b5e0ab12ac11293ecd07e6663a33777610e63
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-07-23 17:02:44 +0530

    Skills Section Added

--- src/components/Skills/helper.js
+++ src/components/Skills/helper.js
@@ -1,20 +1,48 @@
-import Logo from "../../assets/react_logo.png"
+import HTML_LOGO from "../../assets/html_logo.png"
+import CSS_LOGO from "../../assets/css_logo.png"
+import JS_LOGO from "../../assets/js_logo.png"
+import REACT_LOGO from "../../assets/react_logo.png"
+import NODE_LOGO from "../../assets/node_logo.png"
+import MONGO_LOGO from "../../assets/mongo_logo.png"
+import GIT_LOGO from "../../assets/git_logo.png"
+import JAVA_LOGO from "../../assets/java_logo.png"
+import CPP_LOGO from "../../assets/cpp_logo.png"
 const SkillList=[
     {
+        name : "HTML",
+        logo:HTML_LOGO
+    },
+    {
+        name : "CSS",
+        logo:CSS_LOGO
+    },
+    {
+        name : "JS",
+        logo:JS_LOGO
+    },
+    {
         name : "React",
-        logo:Logo
+        logo:REACT_LOGO
     },
     {
         name : "Node Js",
-        logo:Logo
+        logo:NODE_LOGO
     },
     {
-        name : "Express Js",
-        logo:Logo
+        name : "Mongo DB",
+        logo:MONGO_LOGO
     },
     {
-        name : "Git Hub",
-        logo:Logo
+        name : "Git",
+        logo:GIT_LOGO
+    },
+    {
+        name : "JAVA",
+        logo:JAVA_LOGO
+    },
+    {
+        name : "CPP",
+        logo:CPP_LOGO
     }
 ]
 
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,26 +1,32 @@
 import SkillList from "./helper";
-import {LOGO_HEIGHT,LOGO_WIDTH} from '../../constants'
-const Skills=()=>{
-    return(
-        <>
-       <div>
-       <h1 className="text-center text-3xl md:text-4xl lg:text-5xl  font-eurostile font-semibold">Skills</h1>
-      {
-       SkillList.map((val)=>{
-           return(<>
-            <p>{val.name}</p>
-            <img
-              src={val.logo}
-              alt=""
-              height={LOGO_HEIGHT}
-              width={LOGO_WIDTH}
-             className=""
-             />
-           </>)
-       }) 
-      }  
-       </div> 
-        </>
-    )
+import { LOGO_HEIGHT, LOGO_WIDTH } from '../../constants'
+const Skills = () => {
+  return (
+    <>
+      <div>
+        <div className="flex justify-center w-90 " >
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-eurostile font-normal hover:underline">Areas of Expertise</h1>
+        </div>
+        <div className="mt-14 w-100  flex justify-center flex-wrap">
+          {
+            SkillList.map((val) => {
+              return (<>
+                <div className="ml-10 h-50 mr-10 mt-3 hover:text-purple-600 lg:pr-24 lg:pl-24">
+                  <img
+                    src={val.logo}
+                    alt=""
+                    height={LOGO_HEIGHT}
+                    width={LOGO_WIDTH}
+                    className="hover:animate-bounce shadow-md "
+                  />
+                  <p className="text-center text-sm md:text-lg lg:text-xl font-eurostile mt-4 md:mt-8 ">{val.name}</p>
+                </div>
+              </>)
+            })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 export default Skills;
