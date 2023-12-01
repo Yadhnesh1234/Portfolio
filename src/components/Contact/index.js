2bed53c1b17256648f6764e3f3b314dd3f29213e
Yadhnesh1234 <gangurdeyadhnesh28@gmail.com>
2023-12-02 00:40:21 +0530

    Contact Page UI Bug Fix

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,7 @@
-import React, { useState,useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "./style.css"
 
 const Contact = ({ isDarkTheme }) => {
@@ -7,15 +9,16 @@ const Contact = ({ isDarkTheme }) => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
   const form = useRef();
- // const mailvariable = { user_name: "Yadhnesh", user_email: "gangurdeyadhnesh28@gmail.com",message_html: "Hello"}
+  // const mailvariable = { user_name: "Yadhnesh", user_email: "gangurdeyadhnesh28@gmail.com",message_html: "Hello"}
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_owxylh9', 'template_gjs4wpr', form.current , 'df7EvCj1C5We5WumB')
+    emailjs.sendForm("service_owxylh9", "template_gjs4wpr", form.current, "df7EvCj1C5We5WumB")
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
+        toast.success('Thanks for your kind words ! ', { position: toast.POSITION.TOP_CENTER });
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
     // console.log('Form submitted:', name, email, message);
     // Reset the form fields
@@ -26,10 +29,10 @@ const Contact = ({ isDarkTheme }) => {
 
   return (
     <div
-      className={`${
-        isDarkTheme ? 'bg-gray-900 text-white' : 'bg-white text-black'
-      } min-h-screen flex flex-col items-center justify-center`}
+      className={`${isDarkTheme ? 'bg-gray-900 text-white' : 'bg-white text-black'
+        } min-h-screen flex flex-col items-center justify-center`}
     >
+      <ToastContainer />
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-eurostile font-semibold mb-6">
         Say Hello{' '}
         <span
@@ -42,9 +45,8 @@ const Contact = ({ isDarkTheme }) => {
       </h1>
       <form
         ref={form}
-        className={`w-96 md:w-128 p-8 rounded-lg shadow-md ${
-          isDarkTheme ? 'bg-opacity-50' : 'bg-opacity-80'
-        }`}
+        className={`w-96 md:w-128 p-8 rounded-lg shadow-md ${isDarkTheme ? 'bg-opacity-50' : 'bg-opacity-80'
+          }`}
         onSubmit={handleSubmit}
       >
         <div className="mb-6 relative">
@@ -54,9 +56,9 @@ const Contact = ({ isDarkTheme }) => {
           <input
             type="text"
             id="from_name"
-            name="from_name" 
+            name="from_name"
             placeholder="Enter Your Name"
-            className={`w-full py-3 px-4 mt-1 rounded-lg border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:ring-opacity-50 focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'}`}
+            className={`w-full py-3 px-4 mt-1 rounded-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'} border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg`}
             value={name}
             onChange={(e) => setName(e.target.value)}
             required
@@ -70,7 +72,7 @@ const Contact = ({ isDarkTheme }) => {
             type="email"
             id="from_name"
             placeholder="Add your email"
-            className={`w-full py-3 px-4 mt-1 rounded-lg border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:ring-opacity-50 focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'}`}
+            className={`w-full py-3 px-4 mt-1 rounded-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'} border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg`}
             value={email}
             name="from_name"
             onChange={(e) => setEmail(e.target.value)}
@@ -82,22 +84,21 @@ const Contact = ({ isDarkTheme }) => {
             Message
           </label>
           <textarea
-            id="message"
-            rows="6"
-            name="message"
-            placeholder="Enter Message"
-            className={`w-full py-3 px-4 mt-1 rounded-lg border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:ring-opacity-50 focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'}`}
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            required
-          />
+  id="message"
+  rows="6"
+  name="message"
+  placeholder="Enter Message"
+  className={`w-full py-3 px-4 mt-1 rounded-lg focus:outline-none focus:ring focus:ring-${isDarkTheme ? 'green-500' : 'purple-600'} focus:shadow-${isDarkTheme ? 'green-500' : 'purple-600'} border-2 border-${isDarkTheme ? 'green-500' : 'purple-600'} bg-transparent text-lg`}
+  value={message}
+  onChange={(e) => setMessage(e.target.value)}
+  required
+/>
         </div>
         <div className="flex justify-center">
           <button
             type="submit"
-            className={`${
-              isDarkTheme ? 'bg-green-500' : 'bg-purple-600'
-            } text-white py-3 px-6 rounded-lg hover:bg-opacity-90 transition-colors duration-300 text-lg`}
+            className={`${isDarkTheme ? 'bg-green-500' : 'bg-purple-600'
+              } text-white py-3 px-6 rounded-lg hover:bg-opacity-90 transition-colors duration-300 text-lg`}
           >
             Submit
           </button>
